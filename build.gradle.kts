744b862be490df929eb1f9ed3a6c52561b00d81b
laurence <laurence.plaskitt@gymvite.com>
2026-09-12 13:23:32 +0100

    * Added cylonedx plugin to allow gradle to produce SBOMs in the github action. * Added new github action job to run grype against SBOM.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("org.springframework.boot") version "4.1.1"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("plugin.jpa") version "2.3.21"
+    id("org.cyclonedx.bom") version "3.2.4"
 }
 
 group = "com.projects"
